ab4a80ac107b1da98db29ce5c4f3d0b77ffaf83d
Jonah Ingler <jonah@walbots.com>
2016-10-05 16:41:24 -0700

    We added in a none linear response to the sticks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SnoBotOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SnoBotOpMode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by Yanfeng on 9/28/16.
@@ -25,11 +26,52 @@ public class SnoBotOpMode extends OpMode
 
     public void loop()
     {
-        motorLeftWheels.setPower(gamepad1.left_stick_y);
-        motorRightWheels.setPower(gamepad1.right_stick_y);
+        double powerLeftDrive = determinePowerFromInput(gamepad1.left_stick_y);
+        double powerRightDrive = determinePowerFromInput(gamepad1.right_stick_y);
+        motorRightWheels.setPower(powerRightDrive);
+        motorLeftWheels.setPower(powerLeftDrive);
+    }
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
 
 
+
+    double determinePowerFromInput(double dVal)  {
+        double power = dVal;
+
+        // throttle: left_stick_y ranges from -1 to 1, where -1 is full up, and
+        // 1 is full down
+        // direction: left_stick_x ranges from -1 to 1, where -1 is full left
+
+        power = Range.clip(power, -1, 1);
+        power =  (float)scaleInput(power);
+
+        return power;
+    }
 }
-
-
