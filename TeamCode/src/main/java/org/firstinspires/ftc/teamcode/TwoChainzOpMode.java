06e019899d56671e935195472de02b196cfa3784
TeamLaptop <team@walbots.com>
2016-11-06 17:41:53 -0800

    Comments and cleanup. Launch motors temporarily disabled (via comments) until the launch motor controller is installed and configured. Wheel and arm motors tested. Filters for moderating the arm motor movements. Intervals for launch and cooling.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoChainzOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoChainzOpMode.java
@@ -9,59 +9,93 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="TwoChainz", group="Walbots")
 public class TwoChainzOpMode extends OpMode
 {
+    // software-hardware proxy object variables
+
     DcMotor motorLeftWheels;
     DcMotor motorRightWheels;
-    DcMotor motorRotate;
     DcMotor motorAltitude;
+    DcMotor motorRotate;
     DcMotor motorLaunchLeft;
     DcMotor motorLaunchRight;
-    double launchTime = 0f;
-    double coolTime = 0f;
 
+    // timer variables
+
+    double coolTime;
+    double launchTime;
+
+    // constants to tweak certain movements
+
+    static public double FILTER_ALTITUDE    = 0.5f;
+    static public double FILTER_ROTATE      = 0.5f;
+
+    // constants to use for intervals for timers
+
+    static public double INTERVAL_COOLING   = 1f;
+    static public double INTERVAL_LAUNCHING = 1f;
 
     @Override
     public void init()
     {
-        motorLeftWheels = hardwareMap.get(DcMotor.class,"leftwheel");
-        motorRightWheels = hardwareMap.get(DcMotor.class,"rightwheel");
-        motorAltitude = hardwareMap.get(DcMotor.class,"altitude");
-        motorRotate = hardwareMap.get(DcMotor.class,"rotate");
-        //motorLaunchLeft = hardwareMap.get(DcMotor.class,"launchleft");
-        //motorLaunchRight = hardwareMap.get(DcMotor.class,"launchright");
-        motorRightWheels.setDirection(DcMotor.Direction.REVERSE);
+        // grab references to all of the software-hardware proxy objects
+
+        motorLeftWheels  = hardwareMap.get(DcMotor.class, "leftwheel");
+        motorRightWheels = hardwareMap.get(DcMotor.class, "rightwheel");
+        motorAltitude    = hardwareMap.get(DcMotor.class, "altitude");
+        motorRotate      = hardwareMap.get(DcMotor.class, "rotate");
+        //motorLaunchLeft  = hardwareMap.get(DcMotor.class, "launchleft");
+        //motorLaunchRight = hardwareMap.get(DcMotor.class, "launchright");
+
+        // configure the motors to default to the reverse of their typical direction,
+        // to compensate for the motors needing to rotate in concert with their partner motors
+
         //motorLaunchRight.setDirection(DcMotor.Direction.REVERSE);
+        motorRightWheels.setDirection(DcMotor.Direction.REVERSE);
+
+        // reset the timers before their first use
+
+        coolTime   = 0f;
+        launchTime = 0f;
     }
 
     @Override
     public void loop()
     {
-        double powerLeftDrive = determinePowerFromInput(gamepad1.left_stick_y);
-        double powerRightDrive = determinePowerFromInput(gamepad1.right_stick_y);
-        double powerRotate = determinePowerFromInput(gamepad2. left_stick_x);
-        double powerAltitude = determinePowerFromInput(gamepad2.right_stick_y);
+        // compute the power from the appropriate gamepad input
 
-        motorRightWheels.setPower(powerRightDrive);
+        double powerLeftDrive  = determinePowerFromInput(gamepad1.left_stick_y);
+        double powerRightDrive = determinePowerFromInput(gamepad1.right_stick_y);
+        double powerRotate     = determinePowerFromInput(gamepad2. left_stick_x) * FILTER_ROTATE;
+        double powerAltitude   = determinePowerFromInput(gamepad2.right_stick_y) * FILTER_ALTITUDE;
+
+        // apply the computer power
+
         motorLeftWheels.setPower(powerLeftDrive);
+        motorRightWheels.setPower(powerRightDrive);
         motorRotate.setPower(powerRotate);
         motorAltitude.setPower(powerAltitude);
+
+        // print some helpful diagnostic messages to the driver controller app
 
         telemetry.addData("wheels", String.format("left: %.2f\tright: %.2f", powerLeftDrive, powerRightDrive));
         telemetry.addData("arm", String.format("rotate: %.2f\taltitude: %.2f", powerRotate, powerAltitude));
 
+        // if the launch motors aren't cooling off,
+        // trigger the launch motors when gamepad2.right_bumper is pressed
+
         if (gamepad2.right_bumper && coolTime <= time)
         {
-            //motorLaunchLeft.setPower(1f);
-            //motorLaunchRight.setPower(1f);
-            launchTime = time + 1;
-            coolTime = 0f;
+            //motorLaunchLeft.setPower(1f);  // fire launch motor at full power
+            //motorLaunchRight.setPower(1f); // fire launch motor at full power
+            launchTime = time + INTERVAL_LAUNCHING; // set a launchTime to stop the launch motors after
+            coolTime = 0f;         // reset the coolTime for later use
         }
 
         if (launchTime <= time)
         {
-            launchTime = 0f;
-            //motorLaunchLeft.setPower(0f);
-            //motorLaunchRight.setPower(0f);
-            coolTime = time + 1;
+            launchTime = 0f; // reset the launchTime for later use
+            //motorLaunchLeft.setPower(0f);  // turn off the launch motor
+            //motorLaunchRight.setPower(0f); // turn off the launch motor
+            coolTime = time + INTERVAL_COOLING; // set coolTime to prevent the launch motors from burning out from repeated use
         }
     }
 
